96c8bf55d10e2ad4d14fb1236b121338366f1096
Moses374 <emperorskid@gmail.com>
2025-02-13 08:50:30 +0300

    library.hpp modified

--- include/Library.hpp
+++ include/Library.hpp
@@ -15,11 +15,12 @@ using namespace std;
 class Library
 {
 private:
-    map<string, Book> books; // Changed from vector to map (isbn -> Book)
-    vector<Member> members;
+    // Map of name to Member
     TransactionLogger logger;
 
 public:
+    map<string, Book> books; // Map of ISBN to Book
+    map<string, Member> members;
     Library();
 
     void loadBooksFromFile(); // Load at startup
@@ -27,12 +28,15 @@ public:
 
     void addBook(const string &memberID, const string &title, const string &author, const string &genre, const string &isbn, const string &year);
     void removeBook(const string &memberID, const string &isbn);
-    void addMember(const string &name, const string &address, const int &phone);
-    void removeMember(const string &name);
-    void findBook(const string &isbn);
-    void checkOutBook(const string &title, const string &isbn, const string &name); // borrowing a book
-    void checkInBook(const string &isbn, const string &name);                       // returning a book
+    string addMember(const string &name, const string &address, long long contact); // Use long long for contact numbers
+    void removeMember(const string &memberID);
+    void findBook(const string &title);
+    void checkOutBook(const string &isbn, const string &memberID); // borrowing a book
+    void checkInBook(const string &isbn, const string &memberID);  // returning a book
     void display_books();
     void display_members();
+
+    Member *getMember(const string &memberID);
 };
+
 #endif
