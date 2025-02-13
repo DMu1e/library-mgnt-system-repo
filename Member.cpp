c3d1864aec34954b6c18a0abffab319f45128409
jk1g3n <jeremykigen10@gmail.com>
2025-02-13 09:15:54 +0300

    Update Member.cpp

--- Member.cpp
+++ Member.cpp
@@ -1,62 +1,69 @@
-#include "Member.hpp"
-#include "Book.hpp"
-#include "TransactionLogger.hpp"
+#include "../Include/Member.hpp"
+#include "../Include/Book.hpp"
+#include "../Include/TransactionLogger.hpp"
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include <vector>
 
-Member::Member(const string &name, const string &address, const int &contact)
+// Ensure the constructor is defined only once
+Member::Member(const string &name, const string &address, const long long &contact) // Change to const reference
+    : name(name), contact(contact), address(address)                                // Reorder initialization list
 {
-    this->name = name;
-    this->address = address;
-    this->contact = contact;
+    // Generate a unique member ID
+    this->memberID = name.substr(0, 3) + to_string(contact).substr(0, 3); // Use this-> to refer to class member
 }
-string Member::get_name(){return name;}
-string Member::get_address(){return address;}
-int Member::get_contact(){return contact;}
-string Member::get_ID(){return memberID;}
+
+Member::Member() : name(""), contact(0), address(""), memberID("") {} // Reorder initialization list
+
+string Member::get_name() { return name; }
+string Member::get_address() { return address; }
+long long Member::get_contact() { return contact; }
+
 vector<string> Member::get_borrowed_books()
 {
-    for(int i = 0; i < borrowed_books.size(); i++)
-    {
-        cout << borrowed_books[i] << endl;
-    }
     return borrowed_books;
 }
-void Member::set_name(const string &name){this->name = name;}
-void Member::set_address(const string &address){this->address = address;}
-void Member::set_contact(const int &contact){this->contact = contact;}
-void Member::set_ID(const string &ID){this->memberID = ID;}
-void Member::borrowBook(const string &isbn){borrowed_books.push_back(isbn);}
-
-void Member::returnBook(const string &isbn)
-{
-    for(int i = 0; i < borrowed_books.size(); i++)
-    {
-        if(borrowed_books[i] == isbn)
-        {
-            borrowed_books.erase(borrowed_books.begin() + i);
-            return;
-        }
-    }
-    cout << "Book not found" << endl;
+void Member::set_name(const string &name) { this->name = name; }                // Use this-> to refer to class member
+void Member::set_address(const string &address) { this->address = address; }    // Use this-> to refer to class member
+void Member::set_contact(const long long &contact) { this->contact = contact; } // Use this-> to refer to class member
+void Member::set_ID(const string &ID) { this->memberID = ID; }                  // Use this-> to refer to class member
+void Member::borrowBook(const string &isbn) {
+    borrowed_books.push_back(isbn);
 }
+
+void Member::returnBook(const string &isbn) {
+    auto it = find(borrowed_books.begin(), borrowed_books.end(), isbn);
+    if (it != borrowed_books.end()) {
+        borrowed_books.erase(it);
+    } else {
+        cout << "Book not found in borrowed books" << endl;
+    }
+}
+
 bool Member::has_borrowed(const string &isbn)
 {
-    for(int i = 0; i < borrowed_books.size(); i++)
+    for (size_t i = 0; i < borrowed_books.size(); i++) // Change loop variable type to size_t
     {
-        if(borrowed_books[i] == isbn)
+        if (borrowed_books[i] == isbn)
         {
             return true;
         }
     }
     return false;
 }
-void Member::display_borrowed_books()
-{
-    for(int i = 0; i < borrowed_books.size(); i++)
-    {
-        cout << borrowed_books[i] << endl;
+void Member::display_borrowed_books() {
+    if (borrowed_books.empty()) {
+        cout << "No borrowed books" << endl;
+    } else {
+        for (const auto &isbn : borrowed_books) {
+            cout << isbn << endl;
+        }
     }
+}
+
+string Member::getMemberID()
+{
+    return memberID;
 }
