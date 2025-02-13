e0495519105b3e4f94979da731f82ad7ef7a030c
Adam <adam.etyang0@gmail.com>
2025-02-13 06:52:27 +0300

    Update transactionlogger.hpp

--- include/transactionlogger.hpp
+++ include/transactionlogger.hpp
@@ -1,1 +1,25 @@
+#ifndef TRANSACTIONLOGGER_HPP
+#define TRANSACTIONLOGGER_HPP
+
+#include <string>
+#include <fstream>
+#include <stdexcept>
+
+using namespace std;
+
+class TransactionLogger
+{
+private:
+    ofstream logfile;
+    string logfilePath;
+    string get_current_time();
+
+public:
+    TransactionLogger(const std::string &path = "Logs/TransactionLogs.txt");
+    ~TransactionLogger();
+    void logTransaction(const std::string &memberID, const std::string &isbn, const std::string &action);
+    void displayLogs();
+};
+
+#endif
 
